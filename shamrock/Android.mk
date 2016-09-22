be45f905f7d9a30cffebe51f8b3086d1550d2ae0
Wzedlare <vedatak01@gmail.com>
2016-09-22 14:35:00 +0300

    no message

--- shamrock/Android.mk
+++ shamrock/Android.mk
@@ -30,15 +30,6 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libdrmframework
-LOCAL_SRC_FILES_32 := lib/libdrmframework.so
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libimg_utils
 LOCAL_SRC_FILES_32 := lib/libimg_utils.so
 LOCAL_MULTILIB := 32
